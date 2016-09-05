b7e9df21291c7634bfe15110b87b8b082f8f86d3
Jingkai Tang <jingkaitang@gmail.com>
2016-09-05 16:27:47 +0800

    mix: specs something

--- web/index.js
+++ web/index.js
@@ -17,6 +17,8 @@ function generate() {
         },
         success: function(data, status, xhr) {
             if (status == 'success') {
+                addSpec(width, height, line, column);
+                addText(text);
                 success(data.pdf);
             } else {
                 failure();
@@ -39,20 +41,65 @@ function failure() {
 }
 
 function addSpec(width, height, line, column) {
-    spec = {name: "自定义规格", width: width, height: height, line: line, column: column};
-    specs_client.push(spec);
-    Cookies.set(key_specs);
+    var specs = $.merge(specs_server, specs_client).filter(function (spec) {
+        return spec.width == width && spec.height == height && spec.line == line && spec.column == column;
+    });
+    if (specs.length == 0) {
+        var spec = {name: "自定义规格", tag: Date.now(), width: width, height: height, line: line, column: column};
+        specs_client.push(spec);
+        Cookies.set(key_specs, specs_client);
 
-    bindSpecs();
+        addItem(spec, true);
+    }
 }
 
-function removeSpec() {
+function addText(text) {
 
-    bindSpecs();
 }
 
-function bindSpecs() {
+function removeSpec(elem) {
+    var tag = $(elem).parents("li").attr("data-tag");
+    removeItem(tag);
+}
 
+function addItem(spec, delabel) {
+    var template = $("ul#specs-list>li.template");
+    var item = template.clone();
+    item.removeClass("template");
+    item.attr("data-tag", spec.tag);
+    item.find("div>span").text(getSpecDisplayText(spec));
+    if (!delabel) {
+        item.find("div>a").removeAttr("onclick");
+        item.find("div>a").addClass("disabled");
+    }
+    template.after(item);
+}
+
+function removeItem(tag) {
+    $("ul#specs-list>li[data-tag=\"" + tag + "\"]").remove();
+    specs_client = specs_client.filter(function (spec) {
+        return spec.tag != tag;
+    });
+    Cookies.set(key_specs, specs_client);
+}
+
+function getSpecDisplayText(spec) {
+    return spec.name + " 宽" + spec.width + "厘米 高" + spec.height + "厘米 " + spec.line + "行 " + spec.column + "列";
+}
+
+function selectItem(elem) {
+    $("#input-spec").val($(elem).text());
+    var tag = $(elem).parents("li").attr("data-tag");
+    var specs = $.merge(specs_server, specs_client).filter(function (spec) {
+        return spec.tag == tag;
+    });
+    if (specs.length > 0) {
+        var spec = specs[0];
+        $("input[name='width']").val(spec.width);
+        $("input[name='height']").val(spec.height);
+        $("input[name='line']").val(spec.line);
+        $("input[name='column']").val(spec.column);
+    }
 }
 
 $(function() {
@@ -64,6 +111,9 @@ $(function() {
         dataType: "json",
         success: function(data, status, xhr) {
             specs_server = data;
+            specs_server.forEach(function (spec) {
+                addItem(spec, false);
+            });
         }
     });
 
@@ -72,5 +122,7 @@ $(function() {
         specs_client = [];
     }
 
-    bindSpecs();
+    specs_client.forEach(function (spec) {
+        addItem(spec, true);
+    });
 });
